0ff2e04031c1a6d12b0358a8f209143f9b30542d
moshe <lerner1737@gmail.com>
2023-01-30 17:23:10 +0200

    migrated database funcs to seperate file

---
+++ modules/database.py
@@ -0,0 +1,36 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def databaseSheet():
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        SERVICE_ACCOUNT_FILE = './joshCreds.json'
+
+        credentials = None
+        credentials = service_account.Credentials.from_service_account_file(
+                SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+        service = build('sheets', 'v4', credentials=credentials)
+        sheet = service.spreadsheets()
+        return sheet
+
+
+def readDatabase():
+        SPREADSHEET_ID = os.getenv('databaseSpreadsheetId')
+        result = databaseSheet().values().get(spreadsheetId=SPREADSHEET_ID,
+                       range="sheet1!A1:C1").execute()
+
+        values = result.get('values', [])
+        return values[0]
+        
+
+
+def writeToDatabase(updatedMessage, updatedFirst, updatedLast):
+        SPREADSHEET_ID = os.getenv('databaseSpreadsheetId')
+        updatedData = [[updatedMessage, updatedFirst, updatedLast]]
+
+        request = databaseSheet().values().update(spreadsheetId=SPREADSHEET_ID, range="sheet1!A1:C1", valueInputOption="USER_ENTERED", body={"values": updatedData}).execute()
