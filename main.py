fa17bb5f7fa8fa544dd77a6a102124839107917a
moshe <lerner1737@gmail.com>
2023-03-01 20:35:42 +0200

    current state

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import modules.bot as bot
 from PyQt6 import QtWidgets, uic
 
 
-
-
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -39,26 +37,26 @@ class MainWindow(QtWidgets.QMainWindow):
 
    
     def runBlaster(self):
-        storedValues = database.readDatabase()
         database.writeToDatabase(self.message.toPlainText(), str(self.firstRow.value()), str(self.lastRow.value()))
 
         data = gs.fetchData(str(self.firstRow.value()), str(self.lastRow.value()))
       
-        message1 = storedValues[3]
-        message2 = self.message.toPlainText()
+        message = self.message.toPlainText()
+
+        bot.openBrowser()
 
        
         for i in range(len(data)):
-            #if colorcheck():
 
             self.console.append(f'sending message to {data[i][gs.whichColumn(data[i][6])]}...')
 
-            newMessage1 = message1.format(firstName=gs.formatFirstName(data[i][2]))
-            newMessage2 = message2.format(businessName=gs.formatBusinessName(data[i][1]), approvalAmount=gs.roundApproval(data[i][12]))
+            newMessage = message.format(firstName=gs.formatFirstName(data[i][2]), businessName=gs.formatBusinessName(data[i][1]), approvalAmount=gs.roundApproval(data[i][12]))
             
-            bot.sendMessage(eval(newMessage1), gs.formatNumber(data[i][gs.whichColumn(data[i][6])]))
-            bot.sendMessage(eval(newMessage2), gs.formatNumber(data[i][gs.whichColumn(data[i][6])]))
+            
+            bot.sendMessage(eval(newMessage), gs.formatNumber(data[i][gs.whichColumn(data[i][6])]))
            
+        
+        bot.closeBrowser()
 
         
         self.console.append('Done.')
@@ -76,8 +74,11 @@ if __name__ == '__main__':
     MainWindow {background-color: #7FC8F8;}
     """
 
-
     app.setStyleSheet(stylesheet)
     window.show()
     app.exec()
 
+
+
+
+    
---
+++ modules/database.py
@@ -0,0 +1,36 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def databaseSheet():
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        SERVICE_ACCOUNT_FILE = './judaCreds.json'
+
+        credentials = None
+        credentials = service_account.Credentials.from_service_account_file(
+                SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+        service = build('sheets', 'v4', credentials=credentials)
+        sheet = service.spreadsheets()
+        return sheet
+
+
+def readDatabase():
+        SPREADSHEET_ID = '1asfdsgsdgds'
+        result = databaseSheet().values().get(spreadsheetId=SPREADSHEET_ID,
+                       range="sheet1!A1:C1").execute()
+
+        values = result.get('values', [])
+        return values[0]
+        
+
+
+def writeToDatabase(updatedMessage, updatedFirst, updatedLast):
+        SPREADSHEET_ID = 'asffsfafasf'
+        updatedData = [[updatedMessage, updatedFirst, updatedLast]]
+
+        databaseSheet().values().update(spreadsheetId=SPREADSHEET_ID, range="sheet1!A1:C1", valueInputOption="USER_ENTERED", body={"values": updatedData}).execute()
